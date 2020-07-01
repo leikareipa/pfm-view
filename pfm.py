29506feebdf9288e69171c29c469c36ec97df22a
leikareipa <drilledicecream@gmail.com>
2020-07-01 15:49:19 +0300

    Implement converting pixel byte data to float values

--- pfm.py
+++ pfm.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from pathlib import Path
+import struct
 
 pfmData = Path("test.pfm").read_bytes()
 
@@ -16,6 +17,36 @@ pfmConstituents = pfmData.split(b"\x0a")
 pfmType = pfmConstituents[0].decode("ascii")
 [pfmWidth, pfmHeight] = map(lambda binaryValue: int(binaryValue.decode("ascii")), pfmConstituents[1].split(b" "))
 pfmIsLittleEndian = (pfmConstituents[2][0] == 45) # 45 representing the ASCII character '-'.
-pfmPixels = b"\x0a".join(pfmConstituents[3:])
+pfmUserValue = abs(float(pfmConstituents[2]))
+pfmPixelBytes = b"\x0a".join(pfmConstituents[3:])
 
-print(pfmType, pfmWidth, pfmHeight, pfmIsLittleEndian, len(pfmPixels))
+print(pfmType, pfmWidth, pfmHeight, pfmIsLittleEndian, pfmUserValue, len(pfmPixelBytes))
+
+class Color:
+    """RGBA color in the range [0,1] (bounds not enforced)."""
+
+    def __init__(self, red, green, blue, alpha = 1):
+        self.red = red
+        self.green = green
+        self.blue = blue
+        self.alpha = alpha
+
+# Convert the binary pixel data into RGB pixel values. Each pixel consists of
+# red, green, and blue channels (no alpha), and each channel is composed of
+# 4 bytes to be interpreted as a 32-bit float.
+endianness = ("<f" if pfmIsLittleEndian else ">f")
+numColorChannels = 3
+bytesPerChannel = 4
+pfmPixels = []
+for pixel in range(pfmWidth * pfmHeight):
+
+    idxRed   = (pixel * numColorChannels * bytesPerChannel)
+    idxGreen = (idxRed + bytesPerChannel)
+    idxBlue  = (idxGreen + bytesPerChannel)
+
+    pfmPixels.append(Color(red   = struct.unpack(endianness, pfmPixelBytes[idxRed:  (idxRed   + bytesPerChannel)])[0],
+                           green = struct.unpack(endianness, pfmPixelBytes[idxGreen:(idxGreen + bytesPerChannel)])[0],
+                           blue  = struct.unpack(endianness, pfmPixelBytes[idxBlue: (idxBlue  + bytesPerChannel)])[0]))
+
+assert (len(pfmPixels) == (pfmWidth * pfmHeight)),\
+       "Failed to decode PFM pixel data."
