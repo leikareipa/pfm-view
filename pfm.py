a1f4759f3f01e40aa38de9d1c0e767036baaaa61
leikareipa <drilledicecream@gmail.com>
2020-07-01 17:13:46 +0300

    Move PFM data handling into its own class

--- pfm.py
+++ pfm.py
@@ -3,50 +3,73 @@
 from pathlib import Path
 import struct
 
-pfmData = Path("test.pfm").read_bytes()
-
-# A PFM file consists of a header of 3 ASCII lines, followed by a data block
-# of all of the image's pixels. Each of the header's ASCII lines is separated
-# with a Unix carriage return, and the pixel data block is also separated that
-# way from the 3rd ASCII line.
-# 
-# You can find a more complete description of the PFM format at
-# http://www.pauldebevec.com/Research/HDR/PFM/.
-#
-pfmConstituents = pfmData.split(b"\x0a")
-pfmType = pfmConstituents[0].decode("ascii")
-[pfmWidth, pfmHeight] = map(lambda binaryValue: int(binaryValue.decode("ascii")), pfmConstituents[1].split(b" "))
-pfmIsLittleEndian = (pfmConstituents[2][0] == 45) # 45 representing the ASCII character '-'.
-pfmUserValue = abs(float(pfmConstituents[2]))
-pfmPixelBytes = b"\x0a".join(pfmConstituents[3:])
-
-print(pfmType, pfmWidth, pfmHeight, pfmIsLittleEndian, pfmUserValue, len(pfmPixelBytes))
-
 class Color:
     """RGBA color in the range [0,1] (bounds not enforced)."""
 
-    def __init__(self, red, green, blue, alpha = 1):
+    def __init__(self, red = 1 , green = 0, blue = 1, alpha = 1):
         self.red = red
         self.green = green
         self.blue = blue
         self.alpha = alpha
 
-# Convert the binary pixel data into RGB pixel values. Each pixel consists of
-# red, green, and blue channels (no alpha), and each channel is composed of
-# 4 bytes to be interpreted as a 32-bit float.
-endianness = ("<f" if pfmIsLittleEndian else ">f")
-numColorChannels = 3
-bytesPerChannel = 4
-pfmPixels = []
-for pixel in range(pfmWidth * pfmHeight):
+class PFMImage:
+    """Holds data loaded from a PFM image file."""
 
-    idxRed   = (pixel * numColorChannels * bytesPerChannel)
-    idxGreen = (idxRed + bytesPerChannel)
-    idxBlue  = (idxGreen + bytesPerChannel)
+    def __init__(self, sourceFileName = None):
+        self.width = None
+        self.height = None
+        self.isLittleEndian = None
+        self.type = None
+        self.userValue = None
+        self.pixels = None # Will be a list with a Color element for each pixel.
+        self.filename = None
 
-    pfmPixels.append(Color(red   = struct.unpack(endianness, pfmPixelBytes[idxRed:  (idxRed   + bytesPerChannel)])[0],
-                           green = struct.unpack(endianness, pfmPixelBytes[idxGreen:(idxGreen + bytesPerChannel)])[0],
-                           blue  = struct.unpack(endianness, pfmPixelBytes[idxBlue: (idxBlue  + bytesPerChannel)])[0]))
+        if sourceFileName:
+            self.load_from_file(sourceFileName)
 
-assert (len(pfmPixels) == (pfmWidth * pfmHeight)),\
-       "Failed to decode PFM pixel data."
+    def load_from_file(self, pfmFileName):
+        """Populates this instance with data from the given PFM file.
+        
+        A PFM file consists of a header of 3 ASCII lines, followed by a data block
+        of all of the image's pixels. Each of the header's ASCII lines is separated
+        with a Unix carriage return, and the pixel data block is also separated that
+        way from the 3rd ASCII line.
+        
+        You can find a more complete description of the PFM format at
+        http://www.pauldebevec.com/Research/HDR/PFM/."""
+
+        self.filename = pfmFileName
+        pfmData = Path(pfmFileName).read_bytes()
+        pfmConstituents = pfmData.split(b"\x0a")
+
+        self.type = pfmConstituents[0].decode("ascii")
+        [self.width, self.height] = map(lambda binaryValue: int(binaryValue.decode("ascii")), pfmConstituents[1].split(b" "))
+        self.isLittleEndian = (pfmConstituents[2][0] == 45) # 45 representing the ASCII character '-'.
+        self.userValue = abs(float(pfmConstituents[2]))
+
+        # Convert the binary pixel data into RGB pixel values. Each pixel consists of
+        # red, green, and blue channels (no alpha), and each channel is composed of
+        # 4 bytes to be interpreted as a 32-bit float.
+        pfmPixelBytes = b"\x0a".join(pfmConstituents[3:])
+        endianness = ("<f" if self.isLittleEndian else ">f")
+        numColorChannels = 3
+        bytesPerChannel = 4
+
+        self.pixels = [Color() for x in range(self.width * self.height)]
+
+        for pixelIdx in range(self.width * self.height):
+
+            idxRed   = (pixelIdx * numColorChannels * bytesPerChannel)
+            idxGreen = (idxRed + bytesPerChannel)
+            idxBlue  = (idxGreen + bytesPerChannel)
+
+            self.pixels[pixelIdx].red   = struct.unpack(endianness, pfmPixelBytes[idxRed:  (idxRed   + bytesPerChannel)])[0]
+            self.pixels[pixelIdx].green = struct.unpack(endianness, pfmPixelBytes[idxGreen:(idxGreen + bytesPerChannel)])[0]
+            self.pixels[pixelIdx].blue  = struct.unpack(endianness, pfmPixelBytes[idxBlue: (idxBlue  + bytesPerChannel)])[0]
+
+        assert (len(self.pixels) == (self.width * self.height)),\
+               "Failed to decode PFM pixel data."
+
+pfm = PFMImage(sourceFileName = "./test.pfm")
+
+print(pfm.filename, pfm.width, pfm.height, pfm.isLittleEndian, pfm.userValue, len(pfm.pixels))
